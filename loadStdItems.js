be1dbb3e16ba14da3c8e54dc95dac2c439bf02f4
Walt Williams <37713261+WaltWilliams@users.noreply.github.com>
2021-01-06 18:12:26 -0700

    Add files via upload

---
+++ loadStdItems.js
@@ -0,0 +1,7 @@
+$(document).attr("title", "My Portfolio");
+$('#banner').load("./stdText/banner.txt").hide().delay(3000).fadeIn(1500);
+$('#lower_banner').load("./stdText/lower_banner.txt").hide().delay(6000).fadeIn(1000);
+$('#uFooter').load("./stdText/uFooter.txt").hide().delay(9000).fadeIn(1000);
+$('#lFooter').load('./stdText/lFooter.txt')
+$('#menuHere').load("./navigation.html")
+$('#cr').text('\u00A9  ' +  new Date().getFullYear() + ", Walt L. Williams");
