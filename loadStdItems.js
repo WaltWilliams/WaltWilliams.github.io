6fc1f4880b1be8254792e0ff5f59fbfc09b56e96
Walt Williams <37713261+WaltWilliams@users.noreply.github.com>
2021-01-12 09:56:48 -0700

    Delete loadStdItems.js

--- loadStdItems.js
+++
@@ -1,7 +0,0 @@
-$(document).attr("title", "Walt Williams|Portfolio");
-$('#banner').load("./banner.txt").hide().delay(1500).fadeIn(1500);
-$('#lower_banner').load("./lower_banner.txt").hide().delay(3500).fadeIn(1000);
-$('#uFooter').load("./uFooter.txt").hide().delay(6000).fadeIn(1000);
-$('#lFooter').load('./lFooter.txt')
-$('#menuHere').load("./navigation.html")
-$('#cr').text('\u00A9  ' +  new Date().getFullYear() + ", Walt L. Williams " + ' \u2014 ' + " Longmont, Colorado");
