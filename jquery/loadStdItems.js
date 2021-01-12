8a806fba90558da4a77b4f5679c810978e2cc75e
Walt Williams <37713261+WaltWilliams@users.noreply.github.com>
2021-01-12 10:02:22 -0700

    Delete jquery directory

--- jquery/loadStdItems.js
+++
@@ -1,8 +0,0 @@
-$(document).attr("title", "Walt L. Williams");
-$('#banner').html("Walt L. Williams").hide().delay(1500).fadeIn(1500);
-$('#lower_banner').html("Programming Portfolio").hide().delay(3500).fadeIn(1000);
-$('#lower_banner_res').html("Resume").hide().delay(3500).fadeIn(1000);
-$('#uFooter').html("known as woodlandcoders on google play").hide().delay(6000).fadeIn(1000);
-$('#lFooter').html("<a href=\"https://www.suu.edu/cose/csis/\" target=\"_blank\"  style=\"color: white\">Southern Utah University - Computer Science Department</a>")
-$('#menuHere').load("./navigation.html")
-$('#cr').text('\u00A9  ' +  new Date().getFullYear() + ", Walt L. Williams " + ' \u2014 ' + " Longmont, Colorado");
